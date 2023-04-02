20a2b8f2556418252dbb8b85c6db482d4ed413df
Carpos-C137 <95235156+Carpos-C137@users.noreply.github.com>
2023-04-02 16:58:36 -0500

    solucion problema id animal

--- src/Animal.cpp
+++ src/Animal.cpp
@@ -1,12 +1,9 @@
 //
 // Created by carlo on 25/03/2023.
 //
-
 #include "Animal.h"
 
-
-Animal::Animal(string nombre, string familia, string especie, string habitat, string dieta, int edad, int id)
-        : nombre(nombre),familia(familia), especie(especie), habitat(habitat), dieta(dieta), edad(edad), iD(iD){}
+Animal::Animal(string nombre, string familia, string especie, string habitat, string dieta, int edad, int id) : nombre(nombre),familia(familia), especie(especie), habitat(habitat), dieta(dieta), edad(edad), iD(id){}
 
 string Animal::getNombre()
 {
